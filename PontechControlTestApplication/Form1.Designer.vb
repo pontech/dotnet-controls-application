e7ce52338c5304976e6f3b807284737413c8d0f3
Jacob Christ <jacob@pontech.com>
2017-06-03 16:58:56 -0700

    Initial commit

---
+++ PontechControlTestApplication/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Knob1 = New PontechControls.Knob()
+        CType(Me.Knob1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Knob1
+        '
+        Me.Knob1.Location = New System.Drawing.Point(95, 75)
+        Me.Knob1.Name = "Knob1"
+        Me.Knob1.Size = New System.Drawing.Size(100, 100)
+        Me.Knob1.TabIndex = 0
+        Me.Knob1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Knob1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Knob1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Knob1 As PontechControls.Knob
+End Class
